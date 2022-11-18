857184b829c5d285bf7a5b7c8744d27c2844e555
Gerard Jaryczewski <gerard.jaryczewski@ptag.pl>
2022-11-18 09:30:44 +0100

    Demo code

--- scrum-workflow/main.js
+++ scrum-workflow/main.js
@@ -1,114 +1,134 @@
+// Powołuje Zespół Deweloperski
+//
+// Funkcja realizuje następujące wymaganie:
+//
 // Powołujemy następujący Zespół Deweloperski:
-
 //  - Scrum Masterem jest Agaton Biały
 //  - Programiści: Cezary Drążek, Ewelina Faworek, Gerwazy Harcerz
 //  - Testerzy: Izydor Jabłkowski, Karolina Ludmiła Młynarz
 //  - DevOps: Natalia Oreo-Przegródka
 //  - Analityk: Roch Studzienny
-
-function ZwalidujOsobe(osoba) // definicja funckji
+function PowolajZespolDeweloperski(scrumMaster, programisci, testerzy, devopsi, analitycy)
 {
+    PowolajScrumMastera(scrumMaster);
 
-}
-
-function PowolajDewelopera(osoba)
-{
-    ZwalidujOsobe(osoba); // wywołanie funkcja
-}
-
-// const pi = 3.14;
-
-// let inflation = 0.24;
-// inflation = 0.26;
-
-function SprawdzCertyfikatScrum(osoba)
-{
-    const pi = 5.14;
-    console.log(pi);
-}
-
-// x = 4 // przypisanie
-// if (x == y) // porównanie
-
-// String "Gerard Jaryczewski"
-// String 'Gerard Jaryczewski'
-// Number 1 2 3 2.4 4.3 4n
-// Boolean true false
-// Object 
-// Symbol
-
-function PowolajScrumMastera(osoba)
-{
-    WalidujScrumMastera(osoba);
-}
-
-function WalidujScrumMastera(osoba)
-{
-    if (osoba == undefined)
+    for (let i = 0; i <= programisci.length; i++)
     {
-        console.log("WTF?");
-        return;
+        PowolajProgramiste(programisci[i]);
     }
 
-    if (osoba == "Gerard Jaryczewski")
-        ZgodzSie();
-    else
-        NieZgodzSie();
+    for (let i = 0; i <= testerzy.length; i++)
+    {
+        PowolajTestera(testerzy[i]);
+    }
+
+    for (let i = 0; i <= devopsi.length; i++)
+    {
+        PowolajDevOpsa(devopsi[i]);
+    }
+
+    for (let i = 0; i <= analitycy.length; i++)
+    {
+        PowolajAnalityka(analitycy[i]);
+    }
 }
 
-function ZgodzSie()
+let scrumMaster = "Agaton Biały";
+let programisci = [ "Cezary Drążek", "Ewelina Faworek", "Gerwazy Harcerz" ];
+let testerzy = [ "Izydor Jabłkowski", "Karolina Ludmiła Młynarz" ];
+let devopsi = [ "Natalia Oreo-Przegródka" ];
+let analitycy = [ "Roch Studzienny" ];
+
+function PowolajScrumMastera(scrumMaster)
 {
-    console.log("Zgadzam się.");
+    console.log("Scrum Master:");
+    console.log(scrumMaster);
 }
 
-function NieZgodzSie()
+function PowolajProgramiste(programista)
 {
-    console.log("You shall not pass!");
+    console.log("Programista:");
+    console.log(programista);
 }
 
-const pi = 3.14;
-
-let kandydat = "Gerard Jaryczewski";
-PowolajScrumMastera(kandydat);
-
-kandydat = "Gerard Krzysztof Jaryczewski"
-PowolajScrumMastera(kandydat);
-
-kandydat = 7;
-PowolajScrumMastera(kandydat);
-
-kandydat = true;
-PowolajScrumMastera(kandydat);
-
-let kandydaci =
-[
-    "Jakub Drzazga",
-    "Grzegorz Iwanek",
-    "Jerzy Cieśliński"
-];
-
-// PowolajScrumMastera(kandydaci[0]);
-// PowolajScrumMastera(kandydaci[1]);
-// PowolajScrumMastera(kandydaci[2]);
-// PowolajScrumMastera(kandydaci[3]);
-
-for (let i = 0; i < kandydaci.length; i++)
+function PowolajTestera(tester)
 {
-    PowolajScrumMastera(kandydaci[i]);
+    console.log("Tester:");
+    console.log(tester);
 }
 
+function PowolajDevOpsa(devops)
+{
+    console.log("DevOps:");
+    console.log(devops);
+}
+
+function PowolajAnalityka(analityk)
+{
+    console.log("Analityk:");
+    console.log(analityk);
+}
+
+// Mianuje Właściciela Produktu
+//
+// Funkcja realizuje następujące wymaganie:
+//
 // Właścicielem Produktu jest Urszula Wał-Złocista.
+function MianujWlascicielaProduktu(wlasciciel)
+{
+    console.log("Właścicielem Produktu jest:");
+    console.log(wlasciciel);
+}
 
+let wlasciciel = "Urszula Wał-Złocista";
+
+// Definiuje cel projektu
+//
+// Funkcja realizuje następujące wymaganie:
+//
 // Celem projektu jest zbudowanie nowej webowej aplikacji onboardingowej dla organizacji.
+function DefiniujCelProjektu(cel)
+{
+    console.log("Celem projektu jest:");
+    console.log(cel);
+}
 
+let cel = "Zbudowanie nowej webowej aplikacji onboardingowej dla organizacji";
+
+// Definiuje termin projektu
+//
+// Funkcja realizuje następujące wymaganie:
+//
 // Termin wdrożenia projektu: 2023-01-31
+function DefiniujTerminProjektu(termin)
+{
+    console.log("Termin wdrożenia projektu:");
+    console.log(termin);
+}
 
+let termin = "2023-01-31";
+
+// Definiuje budżet projektu
+//
+// Funkcja realizuje następujące wymaganie:
+//
 // Budżet projektu: 100 000 PLN
+function DefiniujBudzetProjektu(kwota, waluta)
+{
+    console.log("Budżet projektu:");
+    console.log(kwota);
+    console.log(waluta);
+}
 
-// UWAGA! Zmiany budżetu wymagają zgody kierownictwa.
+let kwota = 100000;
+let waluta = "PLN";
 
+// Przypisuje zadania do Backlogu
+//
+// Funkcja realizuje następujące wymaganie:
+//
 // Do główych zadań zatwierdzonych w Backlogu Produktu należą:
-
+//
 // 1. Definiowanie kompetencji
 // 2. Rejestrowanie artykułów wiedzy
 // 3. Rejestrowanie trenerów
@@ -116,25 +136,162 @@ for (let i = 0; i < kandydaci.length; i++)
 // 5. Rejestrowanie użytkowników
 // 6. Wybór treningów kompetencji przez użytkowników
 // 7. Listy kontrolne realizacji zadań onboardingu
+function PrzypiszZadanieDoBacklogu(zadanie, backlog, backlogStatus)
+{
+    let x = backlog.length;
 
+    backlog[x] = zadanie;
+    backlogStatus[x] = false;
+}
+
+let backlog =
+[
+    "Definiowanie kompetencji",
+    "Rejestrowanie artykułów wiedzy",
+    "Rejestrowanie trenerów",
+    "Przypisywanie kompetencji do trenerów",
+    "Rejestrowanie użytkowników",
+    "Wybór treningów kompetencji przez użytkowników",
+    "Listy kontrolne realizacji zadań onboardingu"
+];
+
+let backlogStatus =
+[
+    false,
+    false,
+    false,
+    false,
+    false,
+    false,
+    false
+];
+
+// Konczy zadanie
+function ZakonczZadanie(idZadania, backlogStatus)
+{
+    backlogStatus[idZadania] = true;
+}
+
+// Dekomponuj zadanie
+//
+// Funkcja realizuje następujące wymaganie:
+//
 // Główe zadania będą dekomponowane na zadania szczegółowe zgodnie z decyzjami Zespołu.
+function DekomponujZadanie(zadanie)
+{
+    console.log("Dekomponuj zadanie:")
+    console.log(zadanie);
+}
 
+// Wybierz metodę wytwórczą
+//
+// Funkcja realizuje następujące wymaganie:
+//
 // Reguły pracy wytwórczej definiuje framework Scrum.
+function WybierzMetodeWytworcza(metoda)
+{
+    console.log("Reguły pracy wytwórczej definiuje:")
+    console.log(metoda);
+}
 
+let metoda = "Scrum";
+
+// Ustala długość sprintu
+//
+// Funkcja realizuje następujące wymaganie:
+//
 // Ustalamy 2-tygodniowy sprint.
+function UstalDlugoscSprintu(dlugosc, jednostka)
+{
+    console.log("Dłuość sprintu:")
+    console.log(dlugosc);
+    console.log(jednostka);
+}
 
+let dlugoscSprintu = 2;
+let jednostkaDlugosciSprintu = "tyg";
+
+// TODO
+//
 // Zgodnie z frameworkiem Scrum, schemat cyklu pracy w sprincie jest następujący:
-
+//
 // - W dniu 1 Sprintu odbywa się spotkanie Planning.
 // - W dniach od 2 do 9 Sprintu odbywa się spotkanie Daily.
 // - W dniu 10 odbywa się spotkanie Review oraz Retrospctive.
-
+//
 // Kolejne sprinty rozpoczynają się automatycznie po sobie.
 
+// Sprawdza warunki zakończenia projektu
+//
+// Funkcja realizuje następujące wymaganie:
+//
+//
 // Jeżeli w toku dowolnego spotkania Właściciel Produktu stwierdza, że dalsza praca nie ma uzasadnienia biznesowego, przerywamy projekt.
-
+//
 // Projekt może zostać przerwany w przypadku przekroczenia terminu danego przez zarząd lub przekroczenia budżetu.
-
+//
 // Jeśli projekt nie zostanie przerwany, zakończy się po realizacji wszystkich głównych zadań.
+function SprawdzWarunkiZakonczeniaProjektu(czyKoniec, czyKontynuowac, dzis, termin, koszty, budzet, backlogStatus)
+{
+    if (czyKontynuowac == false)
+    {
+        czyKoniec = true;
+        return czyKoniec;
+    }
 
-// Happy end!
+    // BUG
+    //
+    // if (dzis > termin)
+    // {
+    //     koniecProjektu = true;
+    //     return koniecProjektu;
+    // }
+
+    if (koszty > budzet)
+    {
+        czyKoniec = true;
+        return czyKoniec;
+    }
+
+    let zamknieteZadania = 0;
+    for (let i = 0; i < backlogStatus.length; i++)
+    {
+        if (backlogStatus[i] == true)
+        {
+            zamknieteZadania++;
+        }
+    }
+    if (zamknieteZadania == backlogStatus.length)
+    {
+        czyKoniec = true;
+        return czyKoniec;
+    }
+
+    return false; // koniecProjektu = false
+}
+
+// DEMO
+
+PowolajZespolDeweloperski(scrumMaster, programisci, testerzy, devopsi, analitycy);
+MianujWlascicielaProduktu(wlasciciel);
+DefiniujCelProjektu(cel);
+DefiniujTerminProjektu(termin);
+DefiniujBudzetProjektu(kwota, waluta);
+PrzypiszZadanieDoBacklogu("Zadania wdrożeniowe", backlog, backlogStatus);
+WybierzMetodeWytworcza(metoda);
+UstalDlugoscSprintu(dlugoscSprintu, jednostkaDlugosciSprintu);
+
+// Ustalamy stan początkowy
+let koniecProjektu = false;
+let czyKontynuowac = true;
+let dzis = "2023-11-17";
+let koszty = 0;
+koniecProjektu = SprawdzWarunkiZakonczeniaProjektu(koniecProjektu, czyKontynuowac, dzis, termin, koszty, kwota, backlogStatus);
+console.log("Czy koniec projektu?");
+console.log(koniecProjektu); // Spodziewam się false
+
+// Przypadek, gdy Właściciel Peroduktu nie zgadza się na kontynuację
+czyKontynuowac = false;
+koniecProjektu = SprawdzWarunkiZakonczeniaProjektu(koniecProjektu, czyKontynuowac, dzis, termin, koszty, kwota, backlogStatus);
+console.log("Czy koniec projektu?");
+console.log(koniecProjektu); // Spodziewam się true
