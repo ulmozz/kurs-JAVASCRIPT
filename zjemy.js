9d1b837f52eeedf4babfa99459c2d7e48b0a5634
Gerard Jaryczewski <gerard.jaryczewski@ptag.pl>
2022-12-21 17:30:41 +0100

    Dodano skrypt zjemy.js

---
+++ zjemy.js
@@ -0,0 +1,25 @@
+// Produkty moga byc w lodowce lub moga byc zamowione (czekamy na dostawe)
+let lodowka = ['jajka', 'bekon', 'ryba'];
+let zamowione = ['chleb', 'maslo', 'wedlina', 'cebula'];
+
+// Skladniki w przepisie na sniadanie
+let sniadanie = ['jajka', 'bekon', 'cebula'];
+
+function czyZjemy(skladniki, produkty) {
+  // Przyjmujemy pesymistycznie, ze NIE zjemy
+  for (let s = 0; s < skladniki.length; s++) {
+    let zjemy = false; // ...i dla kazdego skladnika sprawdzamy to zalozenie osobno
+    for (let p = 0; p < produkty.length; p++) {
+      if (produkty[p] == skladniki[s]) {
+        zjemy = true;
+      }
+    }
+    if (zjemy == false) return false; // Jesli choc jednego skladnika nie ma, to nie zjemy
+  }
+  return true; // Zostalismy zaskoczeni, wszystkie skladniki znalezione
+}
+
+console.log("Czy zjemy sniadanie tylko z produktow w lodowce, nie czekajac na dostawe?");
+console.log(czyZjemy(sniadanie, lodowka));
+console.log("Czy zjemy sniadanie, jesli poczekamy na dostawe?");
+console.log(czyZjemy(sniadanie, [...lodowka, ...zamowione])); // W ten sposob laczymy dwie tablice w jedna
