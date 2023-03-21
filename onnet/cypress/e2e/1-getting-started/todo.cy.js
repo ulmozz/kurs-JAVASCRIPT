309951416c8fb0cc34142611c0a68116790bd5b5
Gerard Jaryczewski <gerard.jaryczewski@ptag.pl>
2023-03-21 15:53:15 +0100

    Selektory

--- onnet/cypress/e2e/1-getting-started/todo.cy.js
+++ onnet/cypress/e2e/1-getting-started/todo.cy.js
@@ -36,7 +36,7 @@ describe('example to-do app', () => {
 
   it('can add new todo items', () => {
     // We'll store our item text in a variable so we can reuse it
-    const newItem = 'Feed the cat'
+    const newItem = 'Feed the elephant'
 
     // Let's get the input element and use the `type` command to
     // input our new list item. After typing the content of our item,
--- onnet/cypress/e2e/newsroom.cy.js
+++
@@ -1,7 +0,0 @@
-describe('template spec', () => {
-  it('passes', () => {
-    cy.visit('http://localhost:3000')
-
-    cy.get('.navbar-brand h1 span').eq(0).should('have.text', "News")
-  })
-})
