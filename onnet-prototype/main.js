5fd52b98a611e98261d56d5cf04445434c994f78
Dariusz Ulma <ulmozz@gmail.com>
2025-05-30 14:13:51 +0200

    Dodanie onnet-prototype

---
+++ onnet-prototype/main.js
@@ -0,0 +1,19 @@
+const fs = require('fs'); // file system
+const http = require('http'); // Hypertext Transfer Protocol
+
+const port = 3000;
+// URL = Uniform Resource Locator ~ adres
+http.createServer(function (req, res) { // request (żądanie), response (odpowiedź)
+  let path = __dirname + '/views/' + req.url + '.html';
+  fs.readFile(path, function (err,data) {
+    if (err) { // if (err != null) {
+      res.writeHead(404);
+      res.end(JSON.stringify(err));
+      return;
+    }
+    res.writeHead(200);
+    res.end(data);
+  });
+}).listen(port);
+
+console.log('Listening on port ' + port); // console.log(`Listening on port ${port}`);
