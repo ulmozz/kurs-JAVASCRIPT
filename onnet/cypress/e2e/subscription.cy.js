8e445cf17335eef97305ae4a9143acdb9cfdfde6
Dariusz Ulma <ulmozz@gmail.com>
2025-06-12 12:24:27 +0200

    Rozszerzono testy subskrypcji

--- onnet/cypress/e2e/subscription.cy.js
+++ onnet/cypress/e2e/subscription.cy.js
@@ -1,11 +1,36 @@
-describe('our example', () => describe('our example', () => {
+describe('subscription features', () => {
   beforeEach(() => {
-    cy.visit('https://example.cypress.io/cypress-api')
+    cy.visit('http://localhost:3000/')
   })
 
-  it('contains link to GitHub repository', () => {
+  it('contains subs text', () => {
     //cy.pause();
-    cy.contains('GitHub');
+    cy.contains('Aliqu justo et labore at eirmod justo sea erat diam dolor diam vero kasd');
   })
-}))
+
+  it('contains unique element containing subs text', () => {
+    //cy.pause();
+    cy.get('#subscription-text');
+    cy.get('[data-test-id="subscription-text"]')
+  })
+
+  it('contains link for reading more', () => {
+    //cy.pause();
+            // Test nam wywala błąd po wejsciu na strone yarrl.pl więc musimy go tu obsłużyć żeby go ignorował
+            cy.origin('https://www.yarrl.pl', () => {
+                cy.on('uncaught:exception', (err) => {
+                // Ignoruj znane błędy JS ze strony yarrl
+                if (err.message.includes('$ is not defined')) {
+                    return false;
+                    }
+                });
+            });
+            // Tu się kończy obsługa błędu
+    cy.get('[data-test-id="subscription-text"] a').click();
+    
+    cy.origin('https://www.yarrl.pl', () => {
+            cy.url().should('include','yarrl')
+            });
+  })
+})
 
